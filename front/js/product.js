c9c21851597ba3ebdbeb9b3dcb928d864f4b25ff
Samy572 <samy.chabbi@gmx.fr>
2022-06-20 21:01:12 +0200

    Connexion de la page d'accueil avec la page produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,9 +1,12 @@
-// Récupération des id
-const picture = document.getElementsByClassName('item_img');
-const title = document.getElementById('title');
+// Récupération des id et des classes dans le html
+const picture = document.querySelector('.item__img');
+const title = document.querySelector('#title');
 const price = document.getElementById('price');
 const description = document.getElementById('description');
-const color = document.getElementById('colors');
+const color = document.querySelector('#colors');
+const option = document.getElementsByTagName('<option>');
+const btn = document.querySelector('#addToCart');
+const quantity = document.querySelector('#quantity');
 
 // récuperer l'id de l'url
 const myId = window.location.search;
@@ -11,4 +14,27 @@ console.log(myId);
 const urlParams = new URLSearchParams(myId);
 
 const _id = urlParams.get('_id');
+// Afficher l'id du produit
 console.log(_id);
+
+fetch('http://localhost:3000/api/products' + '?' + _id)
+	.then((response) => response.json())
+	.then((data) => {
+		console.log(data);
+		data.forEach((element) => {
+			if (_id === element._id) {
+				picture.innerHTML = `<img src="${element.imageUrl}" alt="${element.altTxt}">`;
+				title.innerText = `${element.name}`;
+				price.innerText = `${element.price}`;
+				description.innerText = `${element.description}`;
+				element.colors.forEach((col) => {
+					color.innerHTML += `<option value="${col}"> ${col}</option>`;
+				});
+				price.textContent = `${element.price}`
+				
+			}
+		
+		});
+	});
+
+	
--- front/js/script.js
+++ front/js/script.js
@@ -1,4 +1,3 @@
-
 // récupération de la section items
 let items = document.getElementById('items');
 
@@ -6,17 +5,15 @@ let items = document.getElementById('items');
 fetch('http://localhost:3000/api/products')
 	.then((response) => response.json())
 	.then((data) => {
-    console.log(data);
-    // Boucle pour créer du html dans notre index et afficher les éléments choisi de l'api 
-    data.forEach((element) => {
-      items.innerHTML += `<a href="./product.html?_id=${element._id}">
+		console.log(data);
+		// Boucle pour créer du html dans notre index et afficher les éléments choisi de l'api
+		data.forEach((element) => {
+			items.innerHTML += `<a href="./product.html?_id=${element._id}">
         <article>
           <img src="${element.imageUrl}" alt="${element.altTxt}">
           <h3 class="productName">${element.name}</h3>
           <p class="productDescription">${element.description}</p>
         </article>
       </a>`;
-    });
-   
+		});
 	});
-
